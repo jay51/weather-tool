dcf3a3a4c79c983f22c65821dadc5957b6594bfa
jay51 <nenojaafar3@gmail.com>
2019-01-17 22:34:55 -0600

    implemented making http requests to the weather api and we'r getting the data back

--- app.py
+++ app.py
@@ -4,30 +4,39 @@ import sys
 import requests
 import argparse
 
-#print("arg", sys.argv)
+parser = argparse.ArgumentParser(
+    prog="Weather-tool", description="Display weather results in your area")
 
-parser = argparse.ArgumentParser(description="Display weather results in your area")
-#positional arg city. positional means required without a flag 
+# positional arg city. positional means required without a flag
 parser.add_argument("city", help="get weather for spescified city", type=str)
 
+# Optional arg. if not passed, By default = False
+parser.add_argument(
+    "-s", "--save", help="save your city for next time", action="store_true")
 
-#Optional arg. By default = None
-parser.add_argument("-s", "--save", help="save your city for next time", action="store_true")
-
-#parse arguments
+# parse arguments
 args = parser.parse_args()
 
-#get the arg with the name city
-print(args.save)
-
-
-
-
-
-url = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=" 
+city = args.city
+save = args.save
+# for testing
 key = "57720d9a6c316db99681b89f9302a6d1"
 
-def getWeather():
-	#Request data from api
-	#parse data to python dict
-	#Return what's needed from data
+
+def getWeather(city, key=key):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}"
+    r = requests.get(url)
+    json = r.json()
+    # print(json)
+    # parse data to python dict
+    for item in json:
+        print(f"{item} =>{json[item]}")
+    # Return what's needed from data
+
+
+def main():
+    getWeather(city)
+
+
+if __name__ == "__main__":
+    main()
