5052d0a5e7febf76ab08988a6d6b71e36e5a200a
jay51 <nenojaafar3@gmail.com>
2019-01-18 16:54:54 -0600

    added argument to take in api key

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!./env/bin/python3
 
 import sys
+import time
 import requests
 import argparse
 
@@ -10,9 +11,15 @@ parser = argparse.ArgumentParser(
 # positional arg city. positional means required without a flag
 parser.add_argument("city", help="get weather for spescified city", type=str)
 
-# Optional arg. if not passed, By default = False
+# Optional arg. defaults to False
 parser.add_argument(
     "-s", "--save", help="save your city for next time", action="store_true")
+
+#nargs == Number of arguments for one action.
+#defaults to none else you pass a --key flag and an api key (--key apikey)
+parser.add_argument(
+    "--key", help="openweathermap api key", type=str, nargs=1)
+
 
 # parse arguments
 args = parser.parse_args()
@@ -21,7 +28,12 @@ city = args.city
 save = args.save
 # for testing
 key = "57720d9a6c316db99681b89f9302a6d1"
+local_time = time.localtime(time.time())
+print(local_time.tm_hour)
 
+
+print(args.key)
+print(args.save)
 
 def getWeather(city, key=key):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}"
