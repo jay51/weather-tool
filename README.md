96255f033efdfb6476630ce9c7498b670173558c
jay51 <nenojaafar3@gmail.com>
2019-01-19 16:26:31 -0600

    fixed issue with 2 words cities

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # weather-tool
+
 A CLI weather tool to display the weather in your area.
 
 #### How it works?
-- [X] Make a request to weather app
-- [X] Parse the incoming data
-- [X] Display the data in a nice formate
 
-#### How to install
+- [x] Make a request to weather api
+- [x] Parse the incoming data
+- [x] Display the data in a nice formate
+
+#### How to install?
+
 1. Clone this repo
 2. `cd` into project
 3. Create a virtual environment `python3 -m venv env`
 4. Activate virtual environment `source ./env/bin/activate`
 5. install requirments.txt `pip3 install -r requirements.txt` and now you can run `./app.py`
+
+> **Note:** To **Run it from anywhere** add `app.py` to path or Create a link to `~/bin` . inside repo `dir` type `ln app.py /usr/local/bin/<pickName>`.
--- app.py
+++ app.py
@@ -9,14 +9,15 @@ parser = argparse.ArgumentParser(
     prog="Weather-tool", description="Display weather results in your area")
 
 # positional arg city. positional means required without a flag
-parser.add_argument("city", help="get weather for spescified city", type=str)
+parser.add_argument(
+    "city", help="get weather for spescified city", type=str, nargs="*", default="San Antonio")
 
 # Optional arg. defaults to False
 parser.add_argument(
     "-s", "--save", help="save your city for next time", action="store_true")
 
-#nargs == Number of arguments for one action.
-#defaults to none else you pass a --key flag and an api key (--key apikey)
+# nargs == Number of arguments for one action.
+# defaults to none else you pass a --key flag and an api key (--key apikey)
 parser.add_argument(
     "--key", help="openweathermap api key", type=str, nargs=1)
 
@@ -29,26 +30,39 @@ save = args.save
 # for testing
 key = "57720d9a6c316db99681b89f9302a6d1"
 local_time = time.localtime(time.time())
-print(local_time.tm_hour)
+# print(local_time.tm_hour)
 
 
 print(args.key)
+print(args.city)
 print(args.save)
 
-def getWeather(city, key=key):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}"
+
+def getWeather(query, key=key):
+    if type(query) is list:
+        query = "+".join(city)
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={query}&appid={key}"
     r = requests.get(url)
-    json = r.json()
-    # print(json)
     # parse data to python dict
-    for item in json:
-        print(f"{item} =>{json[item]}")
+    json = r.json()
     # Return what's needed from data
+    return {
+        key: json[key] for key in json if key == "name" or key == "weather" or key == "main" or key == "speed"
+    }
 
 
 def main():
-    getWeather(city)
+    weather = getWeather(city)
+    for item in weather:
+        print(f"{item} -> {weather[item]}")
 
 
 if __name__ == "__main__":
     main()
+
+
+"""Todo
+1. work on caching http requests
+2. work on saving city
+3. work on taking api keys 
+"""
