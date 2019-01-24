36ade99acd6ec289891b00bb376a17255354c0bf
jay51 <nenojaafar3@gmail.com>
2019-01-24 14:47:48 -0600

    fixed issue with saving keys and city #1

--- app.py
+++ app.py
@@ -8,38 +8,29 @@ import argparse
 from dotenv import load_dotenv
 
 
-parser = argparse.ArgumentParser(
-    prog="Weather-tool", description="Display weather results in your area")
+def init_arg_parser():
+    parser = argparse.ArgumentParser(
+        prog="Weather-tool", description="Display weather results in your area")
 
-# positional arg city. positional means required without a flag
-parser.add_argument(
-    "city", help="get weather for spescified city", type=str, nargs="*", default="San Antonio")
+    # positional arg city. positional means required without a flag
+    parser.add_argument(
+        "city", help="get weather for spescified city", type=str, nargs="*", default=city)
 
-# Optional arg. defaults to False
-parser.add_argument(
-    "-s", "--save", help="save your city for next time", action="store_true")
+    # Optional arg. defaults to False
+    parser.add_argument(
+        "-s", "--save", help="save your city for next time useage by default", action="store_true")
 
-# nargs == Number of arguments for one action.
-# defaults to none else you pass a --key flag and an api key (--key apikey)
-parser.add_argument(
-    "--key", help="openweathermap api key", type=str)
+    # nargs == Number of arguments for one action.
+    # defaults to none else you pass a --key flag and an api key (--key apikey)
+    parser.add_argument(
+        "--key", help="save your own openweathermap api key", type=str, default=key)
 
-# parse arguments
-args = parser.parse_args()
+    # parse arguments
+    return parser.parse_args()
 
-
-city = args.city
-save = args.save
-key = args.key
 
 # local_time = time.localtime(time.time())
 # print(local_time.tm_hour)
-
-
-print(city)
-print(key)
-# print(save)
-
 
 def getWeather(query, key):
     # units= imeprial for F and units=metric for C and no units will result in kelvins
@@ -64,11 +55,21 @@ def save_to_file(item, value):
 
 
 def main():
+    args = init_arg_parser()
+    key = args.key
+    save = args.save
+    city = "+".join(args.city) if type(args.city) is list else args.city
+    print("++++++++++")
+    print(key)
+    print(city)
+    print(save)
+    print("++++++++++")
 
-    weather = getWeather(city, os.getenv("key"))
+    weather = getWeather(city, key)
+    # save key if key is passed
     if key:
         save_to_file("key", key)
-
+    # save city if city passed
     if city and save:
         save_to_file("city", city)
 
@@ -77,23 +78,23 @@ def main():
     # State and description of weather
     print(
         f'{weather["weather"][0]["main"]} ,{weather["weather"][0]["description"]}')
-    # Temp and humidity and pressure
+    # # Temp and humidity and pressure
     for item in weather["main"]:
         print(f"{item} {weather['main'][item]}")
 
 
 if __name__ == "__main__":
     load_dotenv()
-    # value = os.getenv("key")
-    city = "+".join(city) if type(city) is list else city
+    key = os.getenv("key")
+    city = os.getenv("city")
 
     main()
 
 
 """Todo
 1. work on caching http requests
-2. work on saving city
+X 2. work on saving city
 X 3. work on taking api keys
-4. make argpars arguments in a function because it runs befor load_dotenv() gets called
+X 4. make argpars arguments in a function because it runs befor load_dotenv() gets called
 
 """
